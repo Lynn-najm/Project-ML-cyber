654d897063decccf34b9d55af39118c4565bf964
Lynn Najm <lynnnajm@MacBook-Pro.local>
2026-04-03 23:19:35 +0200

    Add class distribution plot

--- eda.py
+++ eda.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("dataset/Merged01.csv")
 
@@ -7,3 +8,8 @@ print(df.info())
 print(df.shape)
 print(df.columns.tolist())
 print(df['Label'].value_counts())
+
+
+df['Label'].value_counts().plot(kind='bar', figsize=(12,6))
+plt.title("Class Distribution")
+plt.show()
